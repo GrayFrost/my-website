049ecd7b39c5b41c67b7fb715d863b9b6f1e0fd8
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-06-19 15:42:52 +0800

    feat: gpt test

--- src/app/not-found.tsx
+++
@@ -1,5 +0,0 @@
-export default function NotFound() {
-  return (
-    <h1>没有东西，滚！</h1>
-  )
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,29 @@
+'use client'
 import Image from 'next/image'
 import Link from 'next/link';
 import styles from './page.module.css'
+import { useChat } from 'ai/react'
 
 export default function Home() {
-  return (
-    <main className={styles.main}>
-      <div>common</div>
-    </main>
-  )
+  const { messages, input, handleInputChange, handleSubmit } = useChat()
+  return <div>
+    <h2>首页</h2>
+    <div>
+    {messages.map(m => (
+        <div key={m.id}>
+          {m.role}: {m.content}
+        </div>
+      ))}
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Say something...
+          <input
+            value={input}
+            onChange={handleInputChange}
+          />
+        </label>
+      </form>
+    </div>
+  </div>
 }
