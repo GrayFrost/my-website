5317365919462887f4b0165d343b5cb3d82e2d69
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-07-10 14:45:42 +0800

    feat: chart

---
+++ src/app/demo/teacher/page.tsx
@@ -0,0 +1,18 @@
+import Graph from './graph';
+
+export default function Teacher() {
+  return (
+    <div>
+      <h2>转行教师之路</h2>
+      <ul>
+        <li>2023-06-30 普通话等级考试</li>
+        <li>2023-07-07 高中信息技术教师资格证考试报名</li>
+      </ul>
+
+      <section>
+        <h2>知识图谱</h2>
+        <Graph />
+      </section>
+    </div>
+  )
+}
