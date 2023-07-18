2481f04d700923f3c675deca6dab2ba5b157ef5d
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-07-18 17:26:49 +0800

    feat: nav

--- src/app/nav-component.tsx
+++ src/app/nav-component.tsx
@@ -1,20 +1,50 @@
-'use client';
+"use client";
 
-import { useRouter, useParams, usePathname } from 'next/navigation';
-import Link from 'next/link';
+import { useState } from "react";
+import { useRouter, useParams, usePathname } from "next/navigation";
+import Link from "next/link";
 
 export default function NavComponent() {
+  const [navs] = useState([
+    {
+      name: "首页",
+      value: "/",
+    },
+    {
+      name: "博客",
+      value: "/blog",
+    },
+    {
+      name: "演示",
+      value: "/demo",
+    },
+    {
+      name: "生活",
+      value: "/life",
+    },
+    {
+      name: "关于",
+      value: "/about",
+    },
+  ]);
   const router = useRouter();
   const params = useParams();
   const pathname = usePathname();
-  console.log('zzh router信息', router, params, pathname);
-  return pathname === '/' ? (
-    <nav className='flex items-center justify-end px-16 h-[64px] bg-[pink]'>
-      <Link href='/' className='pr-16'>首页</Link>
-      <Link href='/blog' className='pr-16'>博客</Link>
-      <Link href='/demo' className='pr-16'>演示</Link>
-      <Link href='/life' className='pr-16'>生活</Link>
-      <Link href='/about'>关于</Link>
+  console.log("zzh router信息", router, params, pathname);
+  return pathname === "/" ? (
+    <nav className="flex items-center justify-end px-16 h-[64px] bg-[pink]">
+      {navs.map((item, index) => {
+        return (
+          <Link
+            href={item.value}
+            className={`${index !== navs.length - 1 ? "pr-16" : ""}`}
+          >
+            {item.name}
+          </Link>
+        );
+      })}
     </nav>
-  ) : <nav className="h-[64px] bg-gray-400">简化版路由</nav>
+  ) : (
+    <nav className="h-[64px] bg-gray-400">简化版路由</nav>
+  );
 }
