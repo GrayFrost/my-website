4c9bd027b1b3be68fa4755ff15c082ee5d1b20c3
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2023-07-18 16:17:00 +0800

    feat: 分科

--- src/app/demo/teacher/page.tsx
+++ src/app/demo/teacher/page.tsx
@@ -1,7 +1,22 @@
-import Graph from './graph';
-import TeacherMd from './teacher.mdx';
+"use client";
+
+import { useState } from "react";
+import Graph from "./graph";
+import ComprehensiveMd from "./comprehensive.mdx";
+import EducationMd from "./education.mdx";
 
 export default function Teacher() {
+  const [tab, setTab] = useState(1);
+  const [tabs] = useState([
+    {
+      id: 1,
+      name: "综合素质",
+    },
+    {
+      id: 2,
+      name: "教育",
+    },
+  ]);
   return (
     <div>
       <h2>转行教师之路</h2>
@@ -9,6 +24,20 @@ export default function Teacher() {
         <li>2023-06-30 普通话等级考试</li>
         <li>2023-07-07 高中信息技术教师资格证考试报名</li>
       </ul>
+      <div>
+        {tabs.map((item) => {
+          const isActive = tab === item.id;
+          return (
+            <label
+              className={`inline-block w-24 h-8 text-center rounded ${isActive ? 'shadow-md' : ''}`}
+              onClick={() => setTab(item.id)}
+              key={item.id}
+            >
+              {item.name}
+            </label>
+          );
+        })}
+      </div>
       <article className="mx-auto prose prose-slate lg:prose-xl">
         {/* <h2>科目一</h2>
         <h3>素质教育的内涵</h3>
@@ -26,12 +55,13 @@ export default function Teacher() {
           <li>提倡教学从“重结果轻过程"转向“重结果更重过程"</li>
           <li>提倡教学从”关注学科"到“关注人"</li>
         </ul> */}
-        <TeacherMd />
+        {tab === 1 && <ComprehensiveMd />}
+        {tab === 2 && <EducationMd />}
       </article>
       <section>
         <h2>知识图谱</h2>
         <Graph />
       </section>
     </div>
-  )
+  );
 }
