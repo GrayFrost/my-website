dd5c9fdac539eeb524c6dda4760c4358d89475f9
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-07-08 11:10:05 +0800

    feat: icon

--- src/app/svelte/layout.tsx
+++ src/app/svelte/layout.tsx
@@ -1,11 +1,30 @@
+"use client";
+
+import Link from "next/link";
+import { useRouter } from "next/navigation";
+import { HomeIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
+
 export default function Layout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const router = useRouter();
+
   return (
-    <div>
-      {children}
+    <div className="relative">
+      <div className="fixed">
+        <Link href="/">
+          <HomeIcon className="size-6 text-blue-300" />
+        </Link>
+        <ChevronLeftIcon
+          className="size-6 text-blue-300 cursor-pointer"
+          onClick={() => {
+            router.back();
+          }}
+        />
+      </div>
+      <div className="px-6">{children}</div>
     </div>
   );
 }
