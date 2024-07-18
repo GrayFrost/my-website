26fcd90cb983e3a3dfaa4b5ebe9ffeda68fab3c0
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-07-18 14:14:35 +0800

    feat: not ssr

--- src/components/wired-components.tsx
+++ src/components/wired-components.tsx
@@ -6,12 +6,28 @@ import {
 } from "react-wired-elements";
 import type { WiredCardProps, WiredButtonProps } from "react-wired-elements";
 
+function SafeHydrate({ children }: { children: React.ReactNode }) {
+  return (
+    <div suppressHydrationWarning>
+      {typeof window === "undefined" ? null : children}
+    </div>
+  );
+}
+
 const WiredButton = (props: WiredButtonProps) => {
-  return <WiredButtonComponent {...props} />;
+  return (
+    <SafeHydrate>
+      <WiredButtonComponent {...props} />
+    </SafeHydrate>
+  );
 };
 
 const WiredCard = (props: WiredCardProps) => {
-  return <WiredCardComponent {...props} />;
+  return (
+    <SafeHydrate>
+      <WiredCardComponent {...props} />
+    </SafeHydrate>
+  );
 };
 
 export { WiredCard, WiredButton };
