fcc9b4b6d47836bdc0514858ae972bf984b326af
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-07-18 13:27:34 +0800

    feat: remove toc

--- src/app/svelte/[id]/page.tsx
+++ src/app/svelte/[id]/page.tsx
@@ -1,7 +1,5 @@
 import { usePost } from "@/api/posts";
-import { getHeadingsApi } from "@/api/heading";
 import Article from "@/components/article";
-import { TableOfContents } from "@/components/TableOfContents";
 
 export default async function Page({
   params: { id },
@@ -10,14 +8,10 @@ export default async function Page({
 }) {
   const { getPost } = usePost("svelte");
   const { content, data } = await getPost(id);
-  const heading = await getHeadingsApi(id);
   const { description } = data;
   return (
     <div className="flex flex-col items-center">
       <div>{description || ""}</div>
-      <div className="fixed top-16 right-4">
-        <TableOfContents nodes={heading} />
-      </div>
 
       <Article source={content} />
     </div>
