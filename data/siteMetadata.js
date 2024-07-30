88ff756a1ad08963f1adbff1a7eea1cb34fc8987
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-07-30 16:09:05 +0800

    feat: link

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,6 +1,6 @@
 /** @type {import("pliny/config").PlinyConfig } */
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
+  title: '蒜头蒜的空间',
   author: 'Garlic Garlic',
   headerTitle: '蒜头蒜',
   description: 'A blog created with Next.js and Tailwind.css',
@@ -11,8 +11,8 @@ const siteMetadata = {
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
+  email: 'garyfrost4321@gmail.com',
+  github: 'https://github.com/GrayFrost',
   x: 'https://twitter.com/x',
   // twitter: 'https://twitter.com/Twitter',
   facebook: 'https://facebook.com',
@@ -20,6 +20,7 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com',
+  medium: 'https://medium.com/@garyfrost4321',
   locale: 'zh-cn',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
