86e953f4337f3c616d1177481339a14ba53b9414
zhihui123.zhang <zhihui123.zhang@huolala.cn>
2024-07-30 15:06:14 +0800

    feat: site url

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -4,10 +4,10 @@ const siteMetadata = {
   author: 'Garlic Garlic',
   headerTitle: '蒜头蒜',
   description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  language: 'zh-cn',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://z-site-ten.vercel.app',
+  siteRepo: 'https://github.com/GrayFrost/my-website',
   siteLogo: '/static/images/logo.png',
   socialBanner: '/static/images/twitter-card.png',
   mastodon: 'https://mastodon.social/@mastodonuser',
@@ -20,7 +20,7 @@ const siteMetadata = {
   linkedin: 'https://www.linkedin.com',
   threads: 'https://www.threads.net',
   instagram: 'https://www.instagram.com',
-  locale: 'en-US',
+  locale: 'zh-cn',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
